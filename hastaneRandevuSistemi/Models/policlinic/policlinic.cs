2898d552bdd9399f7498517312329c9cb623f17e
İlkem YAMAN <60519336+ilkymn@users.noreply.github.com>
2023-08-02 22:02:42 +0300

    policlinic extra

--- hastaneRandevuSistemi/Models/policlinic/policlinic.cs
+++ hastaneRandevuSistemi/Models/policlinic/policlinic.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,15 @@ namespace hastaneRandevuSistemi.Models.policlinic
 {
     public class policlinic
     {
-        public int policlinicId { get; set; } 
+        public int policlinicId { get; set; }
 
+
+        [Display(Name = "Poliklinik Adı")]
+        [Required(ErrorMessage = "Bu alanı boş bırakamazsınız!")]
         public string policlinicName { get; set; }
 
+        [Display(Name = "Poliklinik Numarası")]
+        [Required(ErrorMessage = "Bu alanı boş bırakamazsınız!")]
         public string policlinicNo { get; set; }
 
 
