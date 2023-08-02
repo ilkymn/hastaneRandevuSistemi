ab03e4e4569b0e083e8d96a0506d2c3131d4aa83
İlkem YAMAN <60519336+ilkymn@users.noreply.github.com>
2023-08-02 19:02:19 +0300

    Update PoliclinicContext.cs

--- hastaneRandevuSistemi/Models/policlinic/PoliclinicContext.cs
+++ hastaneRandevuSistemi/Models/policlinic/PoliclinicContext.cs
@@ -6,7 +6,6 @@ namespace hastaneRandevuSistemi.Models.policlinic
     public class PoliclinicContext : DbContext
     {
         public DbSet<policlinic> Poliklinikler { get; set; }
-
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;
