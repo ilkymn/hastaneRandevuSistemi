1d9fb336c311cb0532f561e5a36dadf1945e5290
İlkem YAMAN <60519336+ilkymn@users.noreply.github.com>
2023-08-03 17:37:19 +0300

    düzeltme

---
+++ hastaneRandevuSistemi/Controllers/randevuAl.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace hastaneRandevuSistemi.Controllers
+{
+    public class randevuAl : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
