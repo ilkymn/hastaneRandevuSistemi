a7af30854bb6e76e995318072586a0c93b53c016
İlkem YAMAN <60519336+ilkymn@users.noreply.github.com>
2023-08-03 17:23:08 +0300

    Update PoliclinicContext.cs

--- hastaneRandevuSistemi/Models/policlinic/PoliclinicContext.cs
+++ hastaneRandevuSistemi/Models/policlinic/PoliclinicContext.cs
@@ -1,17 +1,19 @@
 ﻿using hastaneRandevuSistemi.Models.city;
 using Microsoft.EntityFrameworkCore;
-
 namespace hastaneRandevuSistemi.Models.policlinic
 {
     public class PoliclinicContext : DbContext
     {
-        //public DbSet<policlinic> Poliklinikler { get; set; }
-
-        /*protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        public DbSet<policlinic> Poliklinikler { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;
 			Database=hastaneRandvuSistemi;Trusted_Connection=True;");
-        }*/
+        }
+
+
+
 
     }
 }
+
