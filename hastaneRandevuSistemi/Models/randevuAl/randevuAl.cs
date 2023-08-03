5a2043b005e9607bfca5eb9ff7fb726a17c233c9
İlkem YAMAN <60519336+ilkymn@users.noreply.github.com>
2023-08-03 17:40:29 +0300

    Create randevuAl.cs

---
+++ hastaneRandevuSistemi/Models/randevuAl/randevuAl.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace hastaneRandevuSistemi.Models.randevuAl
+{
+    public class randevuAl
+    {
+        public int hospitalId { get; set; }
+
+
+
+        public int hospitalNumber { get; set; }
+
+
+        [Display(Name = "Hastaneler")]
+        [Required(ErrorMessage = "Bu alanı boş bırakamazsınız!")]
+        public string hospitalName { get; set; }
+
+        public int policlinicId { get; set; }
+
+
+
+        [Display(Name = "Poliklinikler")]
+        [Required(ErrorMessage = "Bu alanı boş bırakamazsınız!")]
+        public string policlinicName { get; set; }
+
+        [Display(Name = "Poliklinik Numarası")]
+        [Required(ErrorMessage = "Bu alanı boş bırakamazsınız!")]
+        public string policlinicNo { get; set; }
+
+
+
+        public int DoctorId { get; set; }
+
+        [Display(Name = "Doktorlar")]
+        [Required(ErrorMessage = "Bu alanı boş bırakamazsınız!")]
+        public string DoctorName { get; set; }
+
+
+    }
+}
