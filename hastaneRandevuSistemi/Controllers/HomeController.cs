a97e0a2ab1878f930f56b403e32b20bfc6285122
beuzasen <ebeyzanurs@gmail.com>
2023-08-07 18:53:58 +0300

    home page fixed

--- hastaneRandevuSistemi/Controllers/HomeController.cs
+++ hastaneRandevuSistemi/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
 ﻿using hastaneRandevuSistemi.Models;
-
-
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -16,24 +12,20 @@ namespace hastaneRandevuSistemi.Controllers
 
     public class HomeController : Controller
     {
-
-        private readonly ILogger<HomeController> _logger;
-        private LanguageService _localization;
-        public HomeController(ILogger<HomeController> logger, LanguageService localization)
-        {
-            _logger = logger;
-            _localization = localization;
-        }
-
-        public HomeController()
-        {
-        }
-
+        //HomeController b = new HomeController();
 
         public IActionResult Index()
         {
             return View();
         }
+
+
+
+        /*[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }*/
 
 
     }
--- hastaneRandevuSistemi/Startup.cs
+++ hastaneRandevuSistemi/Startup.cs
@@ -1,22 +1,12 @@
-<<<<<<< Updated upstream
-using hastaneRandevuSistemi.Services;
-=======
-using hastaneRandevuSistemi.Models;
->>>>>>> Stashed changes
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Localization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
-using System.Reflection;
 using System.Threading.Tasks;
 
 namespace hastaneRandevuSistemi
@@ -32,60 +22,8 @@ namespace hastaneRandevuSistemi
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-
         {
-<<<<<<< Updated upstream
-=======
             services.AddControllersWithViews();
-
->>>>>>> Stashed changes
-            services.AddSingleton<LanguageService>();
-            services.AddLocalization(options => options.ResourcesPath = "Resources");
-
-            services.AddMvc()
-            .AddViewLocalization()
-            .AddDataAnnotationsLocalization(options =>
-            {
-                options.DataAnnotationLocalizerProvider = (type, factory) =>
-                {
-<<<<<<< Updated upstream
-                    var assemblyName = new AssemblyName(typeof(SharedResource).GetTypeInfo().Assembly.FullName);
-                    return factory.Create("SharedResource", assemblyName.Name);
-=======
-                    var assemblyName = new AssemblyName(typeof(ShareResource).GetTypeInfo().Assembly.FullName);
-                    return factory.Create("ShareResource", assemblyName.Name);
->>>>>>> Stashed changes
-                };
-            });
-
-            services.Configure<RequestLocalizationOptions>(
-            options =>
-            {
-                var supportedCultures = new List<CultureInfo>
-                    {
-                            new CultureInfo("tr-TR"),
-                            new CultureInfo("en-US"),
-<<<<<<< Updated upstream
-                            new CultureInfo("de-DE"),
-=======
->>>>>>> Stashed changes
-                    };
-                options.DefaultRequestCulture = new RequestCulture(culture: "tr-TR", uiCulture: "tr-TR");
-                options.SupportedCultures = supportedCultures;
-                options.SupportedUICultures = supportedCultures;
-                options.RequestCultureProviders.Insert(0, new QueryStringRequestCultureProvider());
-            });
-<<<<<<< Updated upstream
-
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies 
-                // is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-=======
->>>>>>> Stashed changes
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -101,9 +39,6 @@ namespace hastaneRandevuSistemi
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            var locOptions = app.ApplicationServices.GetService<IOptions<RequestLocalizationOptions>>();
-            app.UseRequestLocalization(locOptions.Value);
-
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
@@ -111,6 +46,12 @@ namespace hastaneRandevuSistemi
 
             app.UseAuthorization();
 
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
